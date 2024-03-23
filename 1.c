3094f8b76cf01bc183130450e70969dd380d8f47
zhou'xin <zhouxin0307@163.com>
2024-03-23 22:50:41 +0800

    最后一次了

--- 1.c
+++ 1.c
@@ -1,11 +1,13 @@
 #include<stdio.h>
 int main(){
-    char a,b;
+    char a,b,c;
     printf("输入一个小写字母，可转换为大写字母:");
     a=getchar();
     printf("该大写字母为：");
     b=a-32;
     putchar(b);
     printf("\n");
+    printf("按o,回车结束\n");
+    c=getchar();
     return 0;
 }
